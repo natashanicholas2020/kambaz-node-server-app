17219a9ad22f29a28455fb891143a4290f296ec9
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-14 14:04:04 -0400

    need to finish 5.2.3.4

--- Lab5/index.js
+++ Lab5/index.js
@@ -1,5 +1,6 @@
 import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
+import WorkingWithObjects from "./WorkingWithObjects.js";
 
 export default function Lab5(app) {
     app.get("/lab5/welcome", (req, res) => {
@@ -8,5 +9,6 @@ export default function Lab5(app) {
 
     PathParameters(app);
     QueryParameters(app);
+    WorkingWithObjects(app);
   };
   
