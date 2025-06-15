3ace4c321df4f63b822d3eefd6739e723e7faed0
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-14 22:50:29 -0400

    finished 5.2.6.3

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -81,4 +81,15 @@ export default function WorkingWithArrays(app) {
     res.json(todos);
   });
 
+  app.put("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    todos = todos.map((t) => {
+      if (t.id === parseInt(id)) {
+        return { ...t, ...req.body };
+      }
+      return t;
+    });
+    res.sendStatus(200);
+  });
+
 };
