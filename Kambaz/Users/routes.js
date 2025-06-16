953a9b1c5238af97e29c162002dd7270cc0797eb
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-16 12:53:41 -0400

    finished 5.3.2

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -32,8 +32,15 @@ export default function UserRoutes(app) {
     res.json(currentUser);
   };
 
-  const signout = (req, res) => { };
-  const profile = (req, res) => { };
+  const signout = (req, res) => {
+    currentUser = null;
+    res.sendStatus(200);
+  };
+
+  const profile = async (req, res) => {
+    res.json(currentUser);
+  };
+
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
