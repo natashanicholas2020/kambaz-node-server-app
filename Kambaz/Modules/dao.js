bc481ae3d0d2e3ff2090ddb8c7a71d6dce5de61b
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-16 13:48:16 -0400

    finished 5.3.5.1

---
+++ Kambaz/Modules/dao.js
@@ -0,0 +1,6 @@
+import Database from "../Database/index.js";
+
+export function findModulesForCourse(courseId) {
+  const { modules } = Database;
+  return modules.filter((module) => module.course === courseId);
+}
