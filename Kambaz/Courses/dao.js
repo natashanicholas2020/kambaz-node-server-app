09df06d4aa47b908bc39d2bc7c4efa075d898732
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-17 10:50:10 -0400

    finished 6.4.1.1

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -1,4 +1,4 @@
-import Database from "../Database/index.js";
+import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
 
 export function deleteCourse(courseId) {
@@ -9,7 +9,7 @@ export function deleteCourse(courseId) {
   );}  
 
 export function findAllCourses() {
-  return Database.courses;
+    return model.find();
 }
 
 export function findCoursesForEnrolledUser(userId) {
