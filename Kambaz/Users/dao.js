edb6f92879ff037574e48fe1340685c1ec9e9e7a
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-24 19:40:56 -0400

    5.3.2.2

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,29 +1,19 @@
-import model from "./model.js";
+import db from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 
-export const createUser = (user) => {
-    const newUser = { ...user, _id: uuidv4() };
-    return model.create(newUser);  
-}
+let { users } = db;
 
-export const findAllUsers = () => model.find();
+export const createUser = (user) => (users = [...users, { ...user, _id: uuidv4() }]);
 
-export const findUserById = (userId) => model.findById(userId);
+export const findAllUsers = () => users;
 
-export const findUserByUsername = (username) =>  model.findOne({ username: username });
+export const findUserById = (userId) => users.find((user) => user._id === userId);
 
-export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
+export const findUserByUsername = (username) => users.find((user) => user.username === username);
 
-export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
+export const findUserByCredentials = (username, password) =>
+  users.find( (user) => user.username === username && user.password === password );
 
-export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const updateUser = (userId, user) => (users = users.map((u) => (u._id === userId ? user : u)));
 
-export const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role })
-
-export const findUsersByPartialName = (partialName) => {
-    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
-    return model.find({
-      $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
-    });
-  };
-  
+export const deleteUser = (userId) => (users = users.filter((u) => u._id !== userId));
--- index.js
+++ index.js
@@ -1,64 +1,79 @@
-import express from 'express';
-import Hello from "./Hello.js";
+// import express from 'express';
+// import Hello from "./Hello.js";
+// import Lab5 from "./Lab5/index.js";
+// import cors from "cors";
+// import UserRoutes from "./Kambaz/Users/routes.js";
+// import session from "express-session";
+// import "dotenv/config";
+// import CourseRoutes from "./Kambaz/Courses/routes.js";
+// import ModuleRoutes from "./Kambaz/Modules/routes.js";
+// import mongoose from "mongoose";
+// import * as dao from "./Kambaz/Users/dao.js";  // <-- Add this import
+
+// const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
+// mongoose.connect(CONNECTION_STRING);
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.use(
+//   cors({
+//     origin: process.env.NETLIFY_URL || "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+// const sessionOptions = {
+//   secret: process.env.SESSION_SECRET || "kambaz",
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//     sameSite: "lax",
+//     secure: false,
+//   },
+// };
+
+// if (process.env.NODE_ENV !== "development") {
+//   sessionOptions.proxy = true;
+//   sessionOptions.cookie = {
+//     sameSite: "none",
+//     secure: true,
+//     domain: process.env.NODE_SERVER_DOMAIN,
+//   };
+// }
+
+// app.use(session(sessionOptions));
+
+// app.use((req, res, next) => {
+//   console.log("Session:", req.session);
+//   next();
+// });
+
+
+// UserRoutes(app);
+// CourseRoutes(app);
+// // EnrollmentRoutes(app); // Uncomment if you have it
+// ModuleRoutes(app);
+// Lab5(app);
+// Hello(app);
+
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+
+import express from 'express'
+import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kambaz/Users/routes.js";
-import session from "express-session";
-import "dotenv/config";
-import CourseRoutes from "./Kambaz/Courses/routes.js";
-import ModuleRoutes from "./Kambaz/Modules/routes.js";
-import mongoose from "mongoose";
-import * as dao from "./Kambaz/Users/dao.js";  // <-- Add this import
-
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
-mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: process.env.NETLIFY_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-const sessionOptions = {
-  secret: process.env.SESSION_SECRET || "kambaz",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    sameSite: "lax",
-    secure: false,
-  },
-};
-
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.NODE_SERVER_DOMAIN,
-  };
-}
-
-app.use(session(sessionOptions));
-
-app.use((req, res, next) => {
-  console.log("Session:", req.session);
-  next();
-});
-
-
 UserRoutes(app);
-CourseRoutes(app);
-// EnrollmentRoutes(app); // Uncomment if you have it
-ModuleRoutes(app);
 Lab5(app);
-Hello(app);
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(4000);
