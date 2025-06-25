fb7a8242d1e30616fe1636f28d4bd4018ecd8ae9
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-25 10:37:20 -0400

    finished 5.3.5.1

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,20 +1,6 @@
 import Database from "../Database/index.js";
-import { v4 as uuidv4 } from "uuid";
 
-export function updateModule(moduleId, moduleUpdates) {
-    return model.updateOne({ _id: moduleId }, moduleUpdates);
-  }  
-
-export function deleteModule(moduleId) {
-    return model.deleteOne({ _id: moduleId });
-   }   
-
-export function createModule(module) {
-    const newModule = { ...module, _id: uuidv4() };
-    return model.create(newModule);
-  }  
-
-  export function findModulesForCourse(courseId) {
-    return model.find({ course: courseId });
-   }
-   
+export function findModulesForCourse(courseId) {
+  const { modules } = Database;
+  return modules.filter((module) => module.course === courseId);
+}
