b9d54a757d239fca3778ad39caf964e68e2b9325
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-25 09:49:17 -0400

    finished 5.3.3

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 import express from "express";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import session from "express-session";
+import "dotenv/config";
 import UserRoutes from "./Kambaz/Users/routes.js";
 
 const app = express();
-app.use(cors());       
+app.use(cors({
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:5173",
+    }
+));   
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kambaz",
+    resave: false,
+    saveUninitialized: false,
+  };
+
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+
+app.use(session(sessionOptions));
+  
 app.use(express.json());   
 Lab5(app);
 UserRoutes(app);                     
