0639d3a98b399e6825e855e738e2d6f27ca679f9
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-25 10:09:01 -0400

    finished 5.3.4.3

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -5,5 +5,12 @@ export default function CourseRoutes(app) {
     const courses = dao.findAllCourses();
     res.send(courses);
   });
-  
+
+  app.delete("/api/courses/:courseId", (req, res) => {
+    const { courseId } = req.params;
+    const status = dao.deleteCourse(courseId);
+    res.send(status);
+  });
+
+
 }
