85a6a053f7fbfd309d7bb3f543c8bad72b9efa09
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-25 22:50:56 -0400

    finished 6.4.3.4

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -1,5 +1,6 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
+​​import * as enrollmentsDao from "../Enrollments/dao.js";
 
 export default function CourseRoutes(app) {
   app.get("/api/courses", async (req, res) => {
@@ -40,6 +41,10 @@ export default function CourseRoutes(app) {
 
   app.post("/api/courses", async (req, res) => {
     const course = await dao.createCourse(req.body);
+    const currentUser = req.session["currentUser"];
+    if (currentUser) {
+      await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
+    } 
     res.json(course);
   });
  
